adabca92303d0b6889fd86e1ae08db79e9998e2b
jbs <qhtjd9071@naver.com>
2021-05-30 10:51:22 +0900

    vo

--- src/semi/vo/usersVo.java
+++ src/semi/vo/usersVo.java
@@ -1,5 +1,74 @@
 package semi.vo;
 
 public class usersVo {
-
+	private int userNum;
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	private String phone;
+	private int age;
+	private String delUser;
+	public usersVo(int userNum, String id, String pwd, String name, String email, String phone, int age,
+			String delUser) {
+		super();
+		this.userNum = userNum;
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.age = age;
+		this.delUser = delUser;
+	}
+	public usersVo() {}
+	public int getUserNum() {
+		return userNum;
+	}
+	public void setUserNum(int userNum) {
+		this.userNum = userNum;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDelUser() {
+		return delUser;
+	}
+	public void setDelUser(String delUser) {
+		this.delUser = delUser;
+	}
+	
 }
