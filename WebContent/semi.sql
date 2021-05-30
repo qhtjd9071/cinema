c1572ec48274fffd199613b018f8e7f423ab90ef
jbs <qhtjd9071@naver.com>
2021-05-30 12:21:18 +0900

    테이블 수정

--- WebContent/semi.sql
+++ WebContent/semi.sql
@@ -1,4 +1,5 @@
 
+
 /* Drop Tables */
 
 DROP TABLE pay CASCADE CONSTRAINTS;
@@ -21,14 +22,12 @@ DROP TABLE users CASCADE CONSTRAINTS;
 CREATE TABLE book
 (
 	bookNum number NOT NULL,
-	booker varchar2(10) NOT NULL,
 	showNum number NOT NULL,
 	bookDate date NOT NULL,
 	price number NOT NULL,
 	userNum number NOT NULL,
-	cancel varchar2(20) NOT NULL,
-	seatNum number NOT NULL,
-	seatArray varchar2(5) NOT NULL,
+	cancel varchar2(20),
+	seatNum varchar2(10) NOT NULL,
 	PRIMARY KEY (bookNum)
 );
 
@@ -46,8 +45,6 @@ CREATE TABLE comments
 
 CREATE TABLE customer
 (
-	-- 
-	-- 
 	customerNum number NOT NULL,
 	title varchar2(20) NOT NULL,
 	content varchar2(1000) NOT NULL,
@@ -73,32 +70,26 @@ CREATE TABLE event
 
 CREATE TABLE movie
 (
-	-- 
-	-- 
-	showNum number NOT NULL,
+	movieNum number NOT NULL,
 	movieTitle varchar2(50) NOT NULL,
 	movieContent varchar2(1000) NOT NULL,
 	director varchar2(10) NOT NULL,
 	genre varchar2(20) NOT NULL,
 	rating varchar2(20) NOT NULL,
 	image varchar2(100) NOT NULL,
-	PRIMARY KEY (showNum)
+	PRIMARY KEY (movieNum)
 );
 
 
 CREATE TABLE movieComments
 (
-	-- 
-	-- 
 	movieCommentsNum number NOT NULL,
 	id varchar2(20) NOT NULL,
 	content varchar2(200) NOT NULL,
 	star number NOT NULL,
 	writedate date NOT NULL,
-	-- 
-	-- 
-	showNum number NOT NULL,
 	UserNum number NOT NULL,
+    movieNum number NOT NULL,
 	PRIMARY KEY (movieCommentsNum)
 );
 
@@ -140,12 +131,10 @@ CREATE TABLE room
 CREATE TABLE show
 (
 	showNum number NOT NULL,
-	-- 
-	-- 
 	movieNum number NOT NULL,
-	beginTime date NOT NULL,
-	endTime date NOT NULL,
-	showDate date NOT NULL,
+	beginTime varchar2(20) NOT NULL,
+	endTime varchar2(20) NOT NULL,
+    showDate date NOT NULL,
 	roomserialNum number NOT NULL,
 	PRIMARY KEY (showNum)
 );
@@ -160,7 +149,7 @@ CREATE TABLE users
 	email varchar2(50) NOT NULL,
 	phone varchar2(20) NOT NULL,
 	age number NOT NULL,
-	delUser varchar2(20) NOT NULL,
+	delUser varchar2(20),
 	PRIMARY KEY (userNum)
 );
 
@@ -175,14 +164,14 @@ ALTER TABLE pay
 
 
 ALTER TABLE movieComments
-	ADD FOREIGN KEY (showNum)
-	REFERENCES movie (showNum)
+	ADD FOREIGN KEY (movieNum)
+	REFERENCES movie (movieNum)
 ;
 
 
 ALTER TABLE show
 	ADD FOREIGN KEY (movieNum)
-	REFERENCES movie (showNum)
+	REFERENCES movie (movieNum)
 ;
 
 
@@ -228,7 +217,3 @@ ALTER TABLE movieComments
 ;
 
 commit;
-
-
-
-
