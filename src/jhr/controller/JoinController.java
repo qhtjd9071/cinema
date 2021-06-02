34a66cd12810ace9e205d04d80498c85826554ac
jbs <qhtjd9071@naver.com>
2021-06-02 16:05:30 +0900

    테이블 수정

--- src/jhr/controller/JoinController.java
+++ src/jhr/controller/JoinController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import jhr.dao.UsersDao;
-import test.vo.UsersVo;
+import semi.vo.usersVo;
 
 @WebServlet("/join")
 public class JoinController extends HttpServlet{
@@ -27,7 +27,7 @@ public class JoinController extends HttpServlet{
 		String year=req.getParameter("year");
 		String phone=req.getParameter("phone");
 
-		UsersVo vo=new UsersVo(name, id, pwd, email, year, phone);
+		usersVo vo=new UsersVo(name, id, pwd, email, year, phone);
 		UsersDao dao=new UsersDao();
 		int n=dao.insert(vo);
 		
