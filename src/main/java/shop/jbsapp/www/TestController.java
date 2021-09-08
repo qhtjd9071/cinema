f384e08f51a5f941a317b704587e29e29982318c
jbs <qhtjd9071@naver.com>
2021-09-08 19:40:01 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/shop/jbsapp/www/TestController.java
@@ -0,0 +1,13 @@
+package shop.jbsapp.www;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/")
+	public String test() {
+		return "index";
+	}
+}
